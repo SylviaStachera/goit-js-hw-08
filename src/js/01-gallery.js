d07ade19bc713adbca73f801e2f9ce68d8db8c5a
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-03-01 18:36:53 +0100

    Update taks 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+//===================================================
+const gallery = document.querySelector('.gallery');
+
+//Tworzenie div item
+galleryItems.forEach(n => {
+  const galleryItem = `<a class="gallery__item" href="${n.original}">
+  <img
+    class="gallery__image"
+    src="${n.preview}"
+    alt="${n.description}"
+    title="${n.description}"
+  />
+</a>`;
+
+  gallery.insertAdjacentHTML('afterbegin', galleryItem);
+});
+//=======================================================
+// 	//Użycie biblioteki simplelightbox do wyświetlania img na pełnym ekranie i przewijania
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
